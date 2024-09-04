37121e2194c92290736d23e3ac395607336ec8b1
DevLuigi <luigidasilvacoelho@gmail.com>
2024-09-04 20:30:24 -0300

    First commit

---
+++ src/main/java/com/ecoFin/EcoFin/EcoFinApplication.java
@@ -0,0 +1,13 @@
+package com.ecoFin.EcoFin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoFinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoFinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoFin/EcoFin/EcoFinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoFin.EcoFin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoFinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
