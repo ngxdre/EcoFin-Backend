68f2bc67abc145a8c973ffd6a929a5a4704bc56e
DevLuigi <luigidasilvacoelho@gmail.com>
2024-10-20 22:50:56 -0300

    Cfeat: oncluí issues #14, #15 e #16

--- src/main/java/com/ecoFin/EcoFin/EcoFinApplication.java
+++ src/main/java/com/ecoFin/EcoFin/EcoFinApplication.java
@@ -2,8 +2,9 @@ package com.ecoFin.EcoFin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EcoFinApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ecoFin/EcoFin/controller/UserController.java
+++ src/main/java/com/ecoFin/EcoFin/controller/UserController.java
@@ -2,14 +2,16 @@ package com.ecoFin.EcoFin.controller;
 
 import com.ecoFin.EcoFin.domain.user.dto.UserRequestDTO;
 import com.ecoFin.EcoFin.domain.user.dto.UserResponseDTO;
+import com.ecoFin.EcoFin.domain.user.entity.User;
 import com.ecoFin.EcoFin.repository.UserRepository;
 import com.ecoFin.EcoFin.service.user.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("users")
@@ -20,25 +22,31 @@ public class UserController {
     private UserService service;
 
     @GetMapping
-    public List<UserResponseDTO> getAllUsers() {
-        return repository.findAll()
-                .stream()
-                .map(UserResponseDTO::fromUser)
-                .collect(Collectors.toList());
+    public ResponseEntity<List<UserResponseDTO>> listAllUsers() {
+        return ResponseEntity.ok().body(service.listAllUsers());
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUserById(@PathVariable("userId") Long id) {
+        Optional<User> user = service.getUserById(id);
+        return user.isPresent() ? ResponseEntity.ok().body(user) : ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public ResponseEntity<?> saveUser(@RequestBody UserRequestDTO data) {
-        return service.saveUser(UserRequestDTO.newUser(data));
+    public ResponseEntity<Void> saveUser(@Valid @RequestBody UserRequestDTO data) {
+        service.saveUser(UserRequestDTO.newUser(data));
+        return ResponseEntity.ok().build();
     }
 
     @PutMapping("/{userId}")
-    public ResponseEntity<?> updateUserById(@PathVariable("userId") Long userId, @RequestBody UserRequestDTO data) {
-        return service.updateUserById(userId, UserRequestDTO.newUser(data));
+    public ResponseEntity<Void> updateUserById(@PathVariable("userId") Long userId, @Valid @RequestBody UserRequestDTO data) {
+        Optional<User> user = service.updateUserById(userId, UserRequestDTO.newUser(data));
+        return user.isPresent() ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{userId}")
-    public ResponseEntity<?> deleteUserById(@PathVariable("userId") Long userId) {
-        return service.deleteUserById(userId);
+    public ResponseEntity<Void> deleteUserById(@PathVariable("userId") Long userId) {
+        boolean deleted = service.deleteUserById(userId);
+        return deleted ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
     }
 }
