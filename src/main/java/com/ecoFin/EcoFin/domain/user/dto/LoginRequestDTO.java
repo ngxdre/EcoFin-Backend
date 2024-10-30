19d21ebf5338ca412d827aa7cf6ea40b1946b864
André Santos da Silva <andreew.0003@gmail.com>
2024-10-30 17:05:33 -0300

    feat: Conclui issue #18

---
+++ src/main/java/com/ecoFin/EcoFin/domain/user/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.ecoFin.EcoFin.domain.user.dto;
+
+public record LoginRequestDTO (String email, String password){
+}
--- src/main/java/com/ecoFin/EcoFin/repository/UserRepository.java
+++ src/main/java/com/ecoFin/EcoFin/repository/UserRepository.java
@@ -3,4 +3,8 @@ package com.ecoFin.EcoFin.repository;
 import com.ecoFin.EcoFin.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
